44baa9305d5609fae6565dba788374023ec53c7a
Edward Lui <silui@ucsc.edu>
2019-01-28 13:21:04 -0800

    Update views.py

--- code/cowork_space/polls/views.py
+++ code/cowork_space/polls/views.py
@@ -10,7 +10,7 @@ def f(x):
 
 def index(request):
     doodoo = os.environ['wai']
-    return HttpResponse("You suck" + doodoo)
+    return HttpResponse("You dont suck" + doodoo)
 
 def test(request,multipler=2):
     if(multipler>20):
