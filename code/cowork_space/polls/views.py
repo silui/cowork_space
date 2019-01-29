6eed0f130787795a7c1190cfda9ac02264c4853b
Edward Lui <silui@ucsc.edu>
2019-01-29 10:55:31 -0800

    Update views.py

--- code/cowork_space/polls/views.py
+++ code/cowork_space/polls/views.py
@@ -1,10 +1,15 @@
 import os
 from django.http import HttpResponse
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+import math
+
+pool = Pool(processes)
+    
 
 def f(x):
-    return_this=2
-    for doodoo in range(x):
-        return_this=return_this*return_this
+    for doodoo in range (x):
+        return_this=math.factorial(doodoo)
     return return_this
 
 
@@ -13,7 +18,8 @@ def index(request):
     return HttpResponse("Fourth code pipeline test" + doodoo)
 
 def test(request,multipler=2):
-    if(multipler>20):
+    if(multipler>5000):
         return HttpResponse("multipler too big")
-    return HttpResponse("this is the test page\n"+str(f(multipler)))
+    pool.map(f, [multipler,multipler])
+    return HttpResponse("this is the test page")
 
